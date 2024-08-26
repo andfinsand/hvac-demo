66a2c1098d07308eda7f10b49a1698501feb5cee
Andrew Finsand <andfinsand@gmail.com>
2024-08-26 11:19:08 -0700

    Button hover effect for secondary buttons.

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -8,7 +8,7 @@ type PrimaryButtonProps = {
 export default function PrimaryButton({ href, label }: PrimaryButtonProps) {
     return (
         <Link href={href}>
-            <button className="btn-hover-effect rounded-lg font-semibold text-white w-[180px] py-3 hover:text-black">{label}</button>
+            <button className="btn-hover-effect rounded-lg font-semibold text-white w-[180px] py-3 hover:text-black duration-200">{label}</button>
         </Link>
     );
 }
